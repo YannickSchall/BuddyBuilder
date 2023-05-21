00eeaa6c3f6eafa7540b979c3c9c30cd8b9e88e5
ginakokoska <gi161kok@htwg-konstanz.de>
2023-05-21 12:08:02 +0200

    fixed dependecies structure of app

--- buddybuilder/lib/main.dart
+++ buddybuilder/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:buddybuilder/pages/home-page/home-page-view.dart';
-import 'package:buddybuilder/material-theme/color_schemes.g.dart';
+import 'package:buddybuilder/pages/home_page/home_page_view.dart';
+import 'package:buddybuilder/material_theme/color_schemes.g.dart';
 
 void main() async {
   runApp(await buildApp());
