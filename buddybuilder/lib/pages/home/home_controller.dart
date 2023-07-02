9de9dcfc750f3028cef6e03a67fc0b2e3afdc917
YannickSchall <yannickschall@gmx.de>
2023-07-02 13:29:53 +0200

    Added API functionality

--- buddybuilder/lib/pages/home/home_controller.dart
+++ buddybuilder/lib/pages/home/home_controller.dart
@@ -1,27 +1,23 @@
+import 'dart:convert';
 import 'package:buddybuilder/pages/home/home_model.dart';
 import 'package:buddybuilder/pages/home/home_view.dart';
 import 'package:isar/isar.dart';
-import 'package:buddybuilder/db/book_model.dart';
 import 'package:buddybuilder/db/book.dart';
+import 'package:http/http.dart' as http;
 
 class HomeControllerImplementation extends HomeController {
   final Isar isar;
-  late final Stream<int> newestIdStream;
-  bool isNewestIdStreamInitialized = false;
+  late Stream<int> newestIdStream;
 
   HomeControllerImplementation({
     required this.isar,
     HomeModel? model,
   }) : super(model ?? HomeModel(isDarkModeEnabled: false)) {
-    initializeNewestIdStream();
+    _initializeNewestIdStream();
   }
 
-  void initializeNewestIdStream() async {
-    final clientId =
-        await isar.books.where(sort: Sort.desc).anyId().findFirstSync();
-    newestIdStream = _createNewestIdStream(clientId?.id ?? 0);
-    isNewestIdStreamInitialized = true;
-    // Notify listeners or update the state to indicate that the stream is initialized
+  void _initializeNewestIdStream() {
+    newestIdStream = _createNewestIdStream();
   }
 
   @override
@@ -37,37 +33,22 @@ class HomeControllerImplementation extends HomeController {
 
   @override
   void addBook(Book book) async {
-    await isar.writeTxnSync(() async {
-      isar.books.putSync(book);
+    await isar.writeTxn(() async {
+      isar.books.put(book);
     });
   }
-
-  List<Book>? isb() {
-    final b = (isar.books.where().findAllSync());
-    return b;
-  }
-
-  //@override
-  //Future<int> getNewestID() async {
-  //  final clientId = isar.books.where(sort: Sort.desc).anyId().findFirstSync();
-  //  print(clientId);
-  //  return clientId?.id ?? 0;
-  //}
 
   @override
   Stream<int> getNewestID() {
-    if (!isNewestIdStreamInitialized) {
-      initializeNewestIdStream();
-    }
     return newestIdStream;
   }
 
-  Stream<int> _createNewestIdStream(int initialId) {
-    return Stream.periodic(const Duration(seconds: 1), (_) {
+  Stream<int> _createNewestIdStream() {
+    return Stream.periodic(const Duration(milliseconds: 100), (_) async {
       final clientId =
-          isar.books.where(sort: Sort.desc).anyId().findFirstSync();
-      return clientId?.id ?? initialId;
-    });
+          await isar.books.where(sort: Sort.desc).anyId().findFirst();
+      return clientId?.id ?? 0;
+    }).asyncMap((event) async => await event);
   }
 
   @override
@@ -81,6 +62,34 @@ class HomeControllerImplementation extends HomeController {
         await isar.books.deleteSync(id!);
       });
     }
+  }
+
+//----------------------------------------------
+
+  @override
+  Future<String> provideAPIresponse(String param) async {
+    final response = await http.get(
+      Uri.parse(
+          'https://wger.de/api/v2/exercise/search/?language=2&term=' + param),
+    );
+
+    Map<String, dynamic> jsonData = jsonDecode(response.body);
+
+    // Access the decoded data
+
+    final value = jsonData['suggestions'][0]['value'];
+
+    try {
+      int count = jsonData['count'];
+      final results = jsonData['results'];
+      final name = results[0]['name'];
+      String next = jsonData['next'];
+      String previous = jsonData['previous'];
+    } catch (e) {
+      print(e);
+    }
+
+    return value.toString();
   }
 
   @override
--- buddybuilder/lib/pages/home/home_view.dart
+++ buddybuilder/lib/pages/home/home_view.dart
@@ -90,6 +90,24 @@ class HomeView extends ConsumerWidget {
                 }
               },
             ),
+            FutureBuilder<String>(
+              future: controller.provideAPIresponse("ball"),
+              builder: (context, snapshot) {
+                print(snapshot.data);
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  final response = snapshot.data ?? "";
+                  if (response != "") {
+                    return Text(response);
+                  } else {
+                    return const Text('No data found');
+                  }
+                }
+              },
+            ),
             StreamBuilder<int>(
               stream: controller.getNewestID(),
               initialData: 0, // Provide an initial value for newest
@@ -139,4 +157,5 @@ abstract class HomeController extends StateNotifier<HomeModel> {
   void addBook(Book book);
   void clearBooks();
   Stream<int> getNewestID();
+  Future<String> provideAPIresponse(String param);
 }
