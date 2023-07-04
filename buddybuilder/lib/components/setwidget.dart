bcc376a485f6db71669c6ff369d8264b0185d66c
YannickSchall <yannickschall@gmx.de>
2023-07-04 20:47:15 +0200

    finalized the functionality of the setwidget

--- buddybuilder/lib/components/setwidget.dart
+++ buddybuilder/lib/components/setwidget.dart
@@ -24,56 +24,132 @@ class SetWidget extends StatefulWidget {
 }
 
 class _SetWidgetState extends State<SetWidget> {
-  List<Widget> additionalSets = [];
+  List<ValueListenableBuilder<String>> additionalSets = [];
   List<bool> setCompleted = []; // Track completion status of each set
   bool expanded = false;
-  int nextSet = 1;
+  int nextSet = 0;
+  bool isFirstSetGroup = true; // Track if it's the first set group
+
+  @override
+  void initState() {
+    super.initState();
+
+    addSet(); // Call addSet() function once when the widget is created
+  }
 
   void addSet() {
     setState(() {
       nextSet += 1;
-
-      additionalSets.add(
-        Row(
-          children: [
-            Text('Set $nextSet'),
-            const SizedBox(width: 12),
-            Expanded(
-              child: TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  labelText: 'Reps',
+      String inputReps = '';
+      String inputKG = '';
+      final valueNotifier = ValueNotifier('$nextSet');
+      final valueListener = ValueListenableBuilder<String>(
+        valueListenable: valueNotifier,
+        builder: (context, value, child) {
+          int setNr = int.parse(value);
+          return Row(
+            key: Key(nextSet.toString()),
+            children: [
+              if (!isFirstSetGroup) // Check if it's the first set group
+                Container(
+                  color: Colors.grey, // Separator line color
+                  height: 1, // Separator line height
+                ),
+              Text('Set $setNr'),
+              const SizedBox(width: 12),
+              Expanded(
+                child: TextField(
+                  onChanged: (textValue) {
+                    setState(() {
+                      inputReps = textValue;
+                    });
+                  },
+                  controller: TextEditingController(text: inputReps),
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: 'Reps',
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(width: 12),
-            Expanded(
-              child: TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  labelText: 'Weight',
+              const SizedBox(width: 12),
+              Expanded(
+                child: TextField(
+                  onChanged: (textValue) {
+                    setState(() {
+                      inputKG = textValue;
+                    });
+                  },
+                  controller: TextEditingController(text: inputKG),
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: 'Weight',
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(width: 8),
-            IconButton(
-              icon: const Icon(
-                Icons.close,
-                color: Colors.red,
-              ),
-              onPressed: () {
-                setState(() {
-                  nextSet -= 1;
-                  additionalSets.removeLast();
-                  setCompleted.removeLast();
-                });
-              },
-            ),
-          ],
-        ),
+              const SizedBox(width: 8),
+              (nextSet != 1)
+                  ? IconButton(
+                      icon: Icon(
+                        Icons.close,
+                        color: (nextSet != 1) ? Colors.red : Colors.transparent,
+                      ),
+                      onPressed: () {
+                        print("setNr: " + setNr.toString());
+                        if (setNr != 1) {
+                          setState(() {
+                            nextSet -= 1;
+                            additionalSets.removeAt(setNr - 1);
+                            setCompleted.removeAt(setNr - 1);
+                            correctSetNr(setNr - 1);
+                          });
+                        }
+                      },
+                      enableFeedback: (nextSet != 1) ? true : false,
+                      splashColor: (nextSet != 1) ? null : Colors.transparent,
+                    )
+                  : const IconButton(
+                      icon: Icon(Icons.close, color: Colors.transparent),
+                      color: Colors.transparent,
+                      onPressed: null,
+                    ),
+            ],
+          );
+        },
       );
+
+      additionalSets.add(valueListener);
       setCompleted.add(false);
+      isFirstSetGroup =
+          false; // After adding the first set, it's no longer the first set group
     });
+  }
+
+  String getSetNr(int index) {
+    if (additionalSets.isNotEmpty &&
+        index >= 0 &&
+        index < additionalSets.length) {
+      final valueNotifier =
+          additionalSets[index].valueListenable as ValueNotifier<String>;
+      return valueNotifier.value;
+    }
+
+    return 'wrong index or empty list';
+  }
+
+  void updateSetValue(int index, String newValue) {
+    if (additionalSets.isNotEmpty &&
+        index >= 0 &&
+        index < additionalSets.length) {
+      final valueNotifier =
+          additionalSets[index].valueListenable as ValueNotifier<String>;
+      valueNotifier.value = newValue;
+    }
+  }
+
+  void correctSetNr(int currSet) {
+    for (var i = currSet; i < additionalSets.length; i++) {
+      updateSetValue(i, (i + 1).toString());
+    }
   }
 
   void toggleExpanded() {
@@ -98,6 +174,11 @@ class _SetWidgetState extends State<SetWidget> {
             color: Colors.transparent,
             child: Column(
               children: [
+                if (!isFirstSetGroup) // Check if it's the first set group
+                  Container(
+                    color: Colors.grey, // Separator line color
+                    height: 1, // Separator line height
+                  ),
                 Row(
                   children: [
                     Expanded(
@@ -106,7 +187,7 @@ class _SetWidgetState extends State<SetWidget> {
                         child: TextButton(
                           onPressed: toggleExpanded,
                           child: Text(
-                            widget.setTitle ,
+                            widget.setTitle,
                             style: const TextStyle(
                               fontWeight: FontWeight.bold,
                               fontSize: 17,
@@ -123,11 +204,6 @@ class _SetWidgetState extends State<SetWidget> {
                     ),
                   ],
                 ),
-                if (!expanded)
-                  Container(
-                    color: Colors.grey, // Separator line color
-                    height: 1, // Separator line height
-                  ),
               ],
             ),
           ),
@@ -135,51 +211,6 @@ class _SetWidgetState extends State<SetWidget> {
         if (expanded)
           Column(
             children: [
-              Row(
-                children: [
-                  Text('Set ${widget.setNumber + 1}'),
-                  const SizedBox(width: 12),
-                  Expanded(
-                    child: TextFormField(
-                      keyboardType: TextInputType.number,
-                      decoration: const InputDecoration(
-                        labelText: 'Reps',
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 12),
-                  Expanded(
-                    child: TextFormField(
-                      keyboardType: TextInputType.number,
-                      decoration: const InputDecoration(
-                        labelText: 'Weight',
-                      ),
-                    ),
-                  ),
-                  if (widget.setNumber > 1)
-                    IconButton(
-                      icon: const Icon(
-                        Icons.close,
-                        color: Colors.red,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          additionalSets.removeAt(widget.setNumber - 2);
-                          setCompleted.removeAt(widget.setNumber - 2);
-                        });
-                      },
-                    )
-                  else
-                    IconButton(
-                      icon: const Icon(
-                        Icons.close,
-                        color: Colors.transparent,
-                      ),
-                      onPressed: () {},
-                    ),
-                ],
-              ),
-              const SizedBox(height: 8),
               ...additionalSets,
               Align(
                 alignment: Alignment.center,
@@ -194,7 +225,7 @@ class _SetWidgetState extends State<SetWidget> {
                     ),
                     const SizedBox(width: 8),
                     PillButtonWidget(
-                      onPressed: () => widget.onPressed(widget.customId), // Using the provided callback
+                      onPressed: () => widget.onPressed(widget.customId),
                       text: 'Delete Exercise',
                       buttonHeight: 20,
                       buttonWidth: 200,
